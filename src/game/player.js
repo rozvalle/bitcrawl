6f1dabfc5daf53ecb713b51d0f72506f47c330d9
Rozvalle <elmovaleros11@gmail.com>
2025-12-24 13:56:30 +0800

    Add player health properties: initialize hp and maxHp in createPlayer function

--- src/game/player.js
+++ src/game/player.js
@@ -1,4 +1,9 @@
 export function createPlayer(map) {
     const { x, y } = map.getRandomFloorTile();
-    return { x, y };
+    return {
+        x,
+        y,
+        hp: 20,
+        maxHp: 20
+    };
 }
