7a397fe25ea790392a35d8975075549cc4453757
Rozvalle <elmovaleros11@gmail.com>
2025-12-24 12:12:53 +0800

    Refactor sprite assets: add goblin, slime, and bat images for improved game character representation

--- src/game/sprites.js
+++ src/game/sprites.js
@@ -2,17 +2,26 @@
 import floorImg from "../assets/sprites/floor1.png";
 import wallImg from "../assets/sprites/wall1.png";
 import playerImg from "../assets/sprites/player1.png";
+import goblinImg from "../assets/sprites/player1.png";
+import slimeImg from "../assets/sprites/player1.png";
+import batImg from "../assets/sprites/player1.png";
 
 export const sprites = {
     floor: new Image(),
     wall: new Image(),
     player: new Image(),
+    goblin: new Image(),
+    slime: new Image(),
+    bat: new Image()
 };
 
 // Assign the imported URLs to the images
 sprites.floor.src = floorImg;
 sprites.wall.src = wallImg;
 sprites.player.src = playerImg;
+sprites.goblin.src = goblinImg;
+sprites.slime.src = slimeImg;
+sprites.bat.src = batImg;
 
 // Helper to wait for all images to load
 export function loadSprites(onReady) {
